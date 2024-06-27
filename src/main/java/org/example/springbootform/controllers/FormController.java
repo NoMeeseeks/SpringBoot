43e8e2ba103e3237e2422485ada30b07418d5843
Xavier <xavier.e.santamaria.ramirez@gmail.com>
2024-06-26 23:35:55 -0500

    Creacion del metodo handler para recibir y procesar los datos

--- src/main/java/org/example/springbootform/controllers/FormController.java
+++ src/main/java/org/example/springbootform/controllers/FormController.java
@@ -4,18 +4,32 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class FormController {
 
     @GetMapping("/formulario")
     public String form(Model model){
+        model.addAttribute("titulo","Formulario de usuarios");
         return "form";
     }
 
     @PostMapping("/formulario")
-    public String procesarFormulario(Model model){
+    public String procesarFormulario(Model model,
+                                     @RequestParam String usuario,
+                                     @RequestParam String nombre,
+                                     @RequestParam String apellido,
+                                     @RequestParam String email,
+                                     @RequestParam String password){
 
-        return "result";
+        model.addAttribute("titulo","Formulario de usuarios");
+        model.addAttribute("usuario",usuario);
+        model.addAttribute("nombre",nombre);
+        model.addAttribute("apellido",apellido);
+        model.addAttribute("email",email);
+        model.addAttribute("password",password);
+
+        return "resultado";
     }
 }
